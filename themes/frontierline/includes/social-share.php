6987f6d760bb4abc73c8d3e2b981289908901a00
Craig Cook <craigcook@users.noreply.github.com>
2016-11-10 11:35:53 -0800

    Use full permalink for social sharing

--- themes/frontierline/includes/social-share.php
+++ themes/frontierline/includes/social-share.php
@@ -6,7 +6,7 @@
 global $post;
 
 $blog_name = sanitize_text_field(get_bloginfo('name'));
-$share_url = urlencode(wp_get_shortlink($post->ID));
+$share_url = urlencode(get_permalink($post->ID));
 $share_text = urlencode(html_entity_decode(get_the_title($post->ID), ENT_COMPAT, 'UTF-8'));
 $share_via = sanitize_text_field(get_option('frontierline_twitter_username'));
 ?>
