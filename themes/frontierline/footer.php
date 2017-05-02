515458bbc4ae9347fbdcb8bc2b15bd54261dfc39
João Paulo Polles <Joo Paulo Polles>
2017-05-02 11:57:26 -0300

    Retornando ao padrão

--- themes/frontierline/footer.php
+++ themes/frontierline/footer.php
@@ -20,26 +20,39 @@
       <section class="col col-1">
         <div class="logo"><a href="https://www.mozilla.org">Mozilla</a></div>
         <p id="license" class="license">
-        <?php printf(__('Portions of this content are ©1998-%1s by individual contributors. Content available under a <a href="%2s" rel="external license">Creative Commons license</a>', 'frontierline'), date('Y'), esc_attr('https://www.mozilla.org/foundation/licensing/website-content/')); ?>
+        <?php printf(__('Trechos deste conteúdo possuem direitos reservados (©1998-%1s) por colaboradores da mozillabr.org. Conteúdo disponível sob a <a href="%2s" rel="external license">licença Creative Commons</a>', 'frontierline'), date('Y'), esc_attr('https://www.mozilla.org/foundation/licensing/website-content/')); ?>
         </p>
       </section>
 
       <section class="col col-2">
         <ul class="links-join">
-          <li><a href="https://www.mozilla.org/contact/"><?php _e('Contact Us', 'frontierline'); ?></a></li>
-          <li class="wrap"><a class="donate" href="https://donate.mozilla.org/?presets=100,50,25,15&amp;amount=50&amp;currency=usd"><?php _e('Give', 'frontierline'); ?></a></li>
+          <li><a href="#"><?php _e('Sobre', 'frontierline'); ?></a></li>
+          <li class="wrap"><a href="#"><?php _e('Times', 'frontierline'); ?></a></li>
+          <li class="wrap"><a href="#"><?php _e('Projetos', 'frontierline'); ?></a></li>
+          <li class="wrap"><a href="#"><?php _e('Blog', 'frontierline'); ?></a></li>
+          <li><a href="#"><?php _e('Entre em contato', 'frontierline'); ?></a></li>
+          <li class="wrap"><a href="#"><?php _e('Como participar', 'frontierline'); ?></a></li>
+          <li class="clear"><a href="#"><?php _e('Contribua com esse site', 'frontierline'); ?></a></li>
         </ul>
 
         <ul class="links-legal">
-          <li><a href="https://www.mozilla.org/privacy/"><?php _e('Privacy', 'frontierline'); ?></a></li>
+          <li><a href="https://www.mozilla.org/privacy/"><?php _e('Privacidade', 'frontierline'); ?></a></li>
           <li class="wrap"><a href="https://www.mozilla.org/privacy/websites/#cookies"><?php _e('Cookies', 'frontierline'); ?></a></li>
-          <li class="wrap"><a href="https://www.mozilla.org/about/legal/"><?php _e('Legal', 'frontierline'); ?></a></li>
-          <li class="clear"><a href="https://www.mozilla.org/about/legal/fraud-report/"><?php _e('Report Trademark Abuse', 'frontierline'); ?></a></li>
+          <li class="wrap"><a href="https://www.mozilla.org/about/legal/"><?php _e('Jurídico', 'frontierline'); ?></a></li>
+          <li class="clear"><a href="https://www.mozilla.org/about/legal/fraud-report/"><?php _e('Denunciar abuso de marcas registradas', 'frontierline'); ?></a></li>
         </ul>
       </section>
 
       <section class="col col-3">
         <ul class="links-social">
+          <li>
+            Mozilla Brasil:
+            <ul>
+              <li class="wrap"><a href="https://twitter.com/mozillabrasil">Twitter<span> (@mozillabrasil)</span></a></li>
+              <li class="wrap"><a href="https://www.facebook.com/mozillabrasil">Facebook<span> (Mozilla Brasil)</span></a></li>
+              <li class="wrap"><a href="https://www.flickr.com/photos/mozillabrasil/">Flickr<span> (Mozilla)</span></a></li>
+            </ul>
+          </li>
           <li>
             Mozilla:
             <ul>
