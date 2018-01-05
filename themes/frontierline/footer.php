a66f31517d7d437fdc1ca1a386f65a83a82d3413
Craig Cook <craigcook@users.noreply.github.com>
2018-01-04 16:41:49 -0800

    [fix bug 1388189] Update footer links
    Also replace missing icon sprite

--- themes/frontierline/footer.php
+++ themes/frontierline/footer.php
@@ -18,14 +18,14 @@
   <div class="content">
     <nav class="primary">
       <div class="logo">
-        <a href="https://www.mozilla.org" data-link-type="footer" data-link-name="Mozilla">Mozilla</a>
+        <a href="https://www.mozilla.org/?utm_source=blog.mozilla.org&amp;utm_campaign=footer&amp;utm_medium=referral" data-link-type="footer" data-link-name="Mozilla">Mozilla</a>
       </div>
       <section class="mozilla">
-        <h5><a href="https://www.mozilla.org" data-link-type="footer" data-link-name="Mozilla">Mozilla</a></h5>
+        <h5><a href="https://www.mozilla.org/?utm_source=blog.mozilla.org&amp;utm_campaign=footer&amp;utm_medium=referral" data-link-type="footer" data-link-name="Mozilla">Mozilla</a></h5>
         <ul class="mozilla-links">
-          <li><a href="https://www.mozilla.org/about/" data-link-type="footer" data-link-name="About"><?php _e('About', 'frontierline'); ?></a></li>
-          <li><a href="https://www.mozilla.org/contact/" data-link-type="footer" data-link-name="Contact Us"><?php _e('Contact Us', 'frontierline'); ?></a></li>
-          <li><a href="https://donate.mozilla.org/?presets=100,50,25,15&amp;amount=50&amp;currency=usd" class="donate" data-link-type="footer" data-link-name="Donate"><?php _e('Donate', 'frontierline'); ?></a></li>
+          <li><a href="https://www.mozilla.org/about/?utm_source=blog.mozilla.org&amp;utm_campaign=footer&amp;utm_medium=referral" data-link-type="footer" data-link-name="About"><?php _e('About', 'frontierline'); ?></a></li>
+          <li><a href="https://www.mozilla.org/contact/?utm_source=blog.mozilla.org&amp;utm_campaign=footer&amp;utm_medium=referral" data-link-type="footer" data-link-name="Contact Us"><?php _e('Contact Us', 'frontierline'); ?></a></li>
+          <li><a href="https://donate.mozilla.org/?presets=50,30,20,10&amp;amount=30&amp;currency=usd&amp;utm_source=blog.mozilla.org&amp;utm_campaign=footer&amp;utm_medium=referral" class="donate" data-link-type="footer" data-link-name="Donate"><?php _e('Donate', 'frontierline'); ?></a></li>
           <li>
             <ul class="social-links">
               <li><a class="twitter" href="https://twitter.com/mozilla" data-link-type="footer" data-link-name="Twitter (@mozilla)">Twitter<span> (@mozilla)</span></a></li>
@@ -36,12 +36,13 @@
         </ul>
       </section>
       <section class="firefox">
-        <h5><a href="https://www.mozilla.org/firefox/" data-link-type="footer" data-link-name="Mozilla">Firefox</a></h5>
+        <h5><a href="https://www.mozilla.org/firefox/?utm_source=blog.mozilla.org&amp;utm_campaign=footer&amp;utm_medium=referral" data-link-type="footer" data-link-name="Mozilla">Firefox</a></h5>
         <ul class="firefox-links">
-          <li><a href="https://www.mozilla.org/firefox/" data-link-type="footer" data-link-name="Download Firefox now"><?php _e('Download Firefox Web Browser', 'frontierline'); ?></a></li>
-          <li><a href="https://www.mozilla.org/firefox/desktop/" data-link-type="footer" data-link-name="Desktop Browser for Mac, Windows, Linux"><?php _e('Desktop Browser for Mac, Windows, Linux', 'frontierline'); ?></a></li>
-          <li><a href="https://www.mozilla.org/firefox/android/" data-link-type="footer" data-link-name="Mobile Browser for Android"><?php _e('Mobile Browser for Android', 'frontierline'); ?></a></li>
-          <li><a href="https://www.mozilla.org/firefox/ios/" data-link-type="footer" data-link-name="Mobile Browser for iOS"><?php _e('Mobile Browser for iOS', 'frontierline'); ?></a></li>
+          <li><a href="https://www.mozilla.org/firefox/new/?utm_source=blog.mozilla.org&amp;utm_campaign=footer&amp;utm_medium=referral" data-link-type="footer" data-link-name="Download Firefox"><?php _e('Download Firefox', 'frontierline'); ?></a></li>
+          <li><a href="https://www.mozilla.org/firefox/?utm_source=blog.mozilla.org&amp;utm_campaign=footer&amp;utm_medium=referral" data-link-type="footer" data-link-name="Desktop"><?php _e('Desktop', 'frontierline'); ?></a></li>
+          <li><a href="https://www.mozilla.org/firefox/mobile/?utm_source=blog.mozilla.org&amp;utm_campaign=footer&amp;utm_medium=referral" data-link-type="footer" data-link-name="Mobile"><?php _e('Mobile', 'frontierline'); ?></a></li>
+          <li><a href="https://www.mozilla.org/firefox/features/?utm_source=blog.mozilla.org&amp;utm_campaign=footer&amp;utm_medium=referral" data-link-type="footer" data-link-name="Features"><?php _e('Features', 'frontierline'); ?></a></li>
+          <li><a href="https://www.mozilla.org/firefox/channel/desktop/?utm_source=blog.mozilla.org&amp;utm_campaign=footer&amp;utm_medium=referral" data-link-type="footer" data-link-name="Beta, Nightly, Developer Edition"><?php _e('Beta, Nightly, Developer Edition', 'frontierline'); ?></a></li>
           <li>
             <ul class="social-links">
               <li><a class="twitter" href="https://twitter.com/firefox" data-link-type="footer" data-link-name="Twitter (@firefox)">Twitter<span> (@firefox)</span></a></li>
@@ -56,12 +57,12 @@
     <nav class="secondary">
         <div class="small-links">
           <ul>
-            <li><a rel="nofollow" href="https://www.mozilla.org/privacy/" data-link-type="footer" data-link-name="Privacy"><?php _e('Privacy', 'frontierline'); ?></a></li>
+            <li><a rel="nofollow" href="https://www.mozilla.org/privacy/" data-link-type="footer" data-link-name="Privacy"><?php _e('Website Privacy Notice', 'frontierline'); ?></a></li>
             <li><a rel="nofollow" href="https://www.mozilla.org/privacy/websites/#cookies" data-link-type="footer" data-link-name="Cookies"><?php _e('Cookies', 'frontierline'); ?></a></li>
             <li><a rel="nofollow" href="https://www.mozilla.org/about/legal/" data-link-type="footer" data-link-name="Legal"><?php _e('Legal', 'frontierline'); ?></a></li>
           </ul>
           <p class="license">
-          <?php printf(__('Portions of this content are ©1998-%1s by individual contributors. Content available under a <a href="%2s" rel="external license">Creative Commons license</a>', 'frontierline'), date('Y'), esc_attr('https://www.mozilla.org/foundation/licensing/website-content/')); ?>
+          <?php printf(__('Portions of this content are ©1998-%1s by individual contributors. Content available under a <a href="%2s" rel="external license">Creative Commons license</a>.', 'frontierline'), date('Y'), esc_attr('https://www.mozilla.org/foundation/licensing/website-content/')); ?>
           </p>
         </div>
     </nav>
