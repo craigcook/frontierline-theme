11db440cd50984a12ef37c296e28653df76cad35
Stephanie Hobson <sjhobson@gmail.com>
2018-02-13 17:54:11 -0800

    Move custom feed templates from child to parent theme

--- themes/frontierline/functions.php
+++ themes/frontierline/functions.php
@@ -758,4 +758,23 @@ function save_frontierline_display_hero() {
 
 add_action('save_post', 'save_frontierline_display_hero');
 
+
+/**
+ * Use custom feed templates
+ */
+function frontierline_feed_rss2() {
+    load_template( STYLESHEETPATH . '/feeds/feed-rss2.php');
+}
+add_feed('rss2', 'frontierline_feed_rss2');
+
+function frontierline_feed_rdf() {
+    load_template( STYLESHEETPATH . '/feeds/feed-rdf.php');
+}
+add_feed('rdf', 'frontierline_feed_rdf');
+
+function frontierline_feed_atom() {
+    load_template( STYLESHEETPATH . '/feeds/feed-atom.php');
+}
+add_feed('atom', 'frontierline_feed_atom');
+
 ?>
