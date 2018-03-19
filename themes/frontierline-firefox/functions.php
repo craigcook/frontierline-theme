ef5dee3c7e7596a7ba2c4e7f8141d3e3699ce393
Michal Stanke <michal@stanke.cz>
2018-03-19 23:20:26 +0100

    Use wp_enqueue_scripts hook instead of CSS @import of the parent theme (#38)

--- themes/frontierline-firefox/functions.php
+++ themes/frontierline-firefox/functions.php
@@ -8,4 +8,8 @@ function frontierline_fx_setup() {
 endif;
 add_action('after_setup_theme', 'frontierline_fx_setup');
 
-?>
+/* Enqueue Frontierline original stylesheet */
+function frontierline_css() {
+  wp_enqueue_style('frontierline-parent', get_template_directory_uri() . '/style.css');
+}
+add_action('wp_enqueue_scripts', 'frontierline_css');
