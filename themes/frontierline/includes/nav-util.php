a9809409d70e01c3482b5ff156ef8df8fe47bf08
Michal Stanke <michal@stanke.cz>
2018-03-25 17:11:09 +0200

    Hide Explore link when the sidebar is empty

--- themes/frontierline/includes/nav-util.php
+++ themes/frontierline/includes/nav-util.php
@@ -6,7 +6,7 @@
 
 <nav id="nav-util" class="can-stick <?php if (get_theme_mod('frontierline_category_drawer') === '1') : ?>has-categories<?php endif; ?>">
   <ul class="content">
-    <li class="nav-util-sidebar"><a href="#sidebar" aria-controls="sidebar" id="toggle-sidebar"><?php _e('Explore', 'frontierline'); ?></a></li>
+    <li class="nav-util-sidebar"><?php if (is_active_sidebar('sidebar')) : ?><a href="#sidebar" aria-controls="sidebar" id="toggle-sidebar"><?php _e('Explore', 'frontierline'); ?></a><?php endif; ?></li>
    <?php if (get_theme_mod('frontierline_category_drawer') === '1') : ?>
     <li class="nav-util-categories"><a href="#categories" aria-controls="categories" id="toggle-categories"><?php _e('Categories', 'frontierline'); ?></a></li>
    <?php endif; ?>
