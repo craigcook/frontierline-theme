290ec02f7018b33748c0fb2c4130f87fc7456ddd
Craig Cook <craigcook@users.noreply.github.com>
2018-05-23 15:04:03 -0700

    [fix bug 1454271] Make download button persist in sticky header (#47)

--- themes/frontierline/includes/nav-global.php
+++ themes/frontierline/includes/nav-global.php
@@ -14,14 +14,14 @@
         <li class="nav-global-health"><a href="https://www.mozilla.org/internet-health/?utm_source=<?php echo frontierline_blog_domain(); ?>&amp;utm_medium=referral&amp;utm_campaign=blog-nav" rel="external"><?php _e('Internet Health', 'frontierline'); ?></a></li>
         <li class="nav-global-tech"><a href="https://www.mozilla.org/technology/?utm_source=<?php echo frontierline_blog_domain(); ?>&amp;utm_medium=referral&amp;utm_campaign=blog-nav" rel="external"><?php _e('Technology', 'frontierline'); ?></a></li>
         <li class="nav-global-donate"><a href="https://donate.mozilla.org/?presets=50,30,20,10&amp;amount=30&amp;currency=usd&amp;utm_source=<?php echo frontierline_blog_domain(); ?>&amp;utm_medium=referral&amp;utm_campaign=blog-nav" rel="external"><?php _e('Give', 'frontierline'); ?></a></li>
-      <?php if (get_theme_mod('frontierline_firefox_button') === '1') : ?>
-        <li class="nav-global-fxdownload"><a href="https://www.mozilla.org/firefox/new/?utm_source=<?php echo frontierline_blog_domain(); ?>&amp;utm_medium=referral&amp;utm_campaign=blog-nav" rel="external" class="button button-green"><?php _e('Download Firefox', 'frontierline'); ?></a></li>
-      <?php else : ?>
+      <?php if (get_theme_mod('frontierline_firefox_button') != '1') : ?>
         <li class="nav-global-firefox"><a href="https://www.mozilla.org/firefox/?utm_source=<?php echo frontierline_blog_domain(); ?>&amp;utm_medium=referral&amp;utm_campaign=blog-nav" rel="external"><?php _e('Discover Firefox', 'frontierline'); ?></a></li>
       <?php endif; ?>
       </ul>
-    </div>
 
-    <a class="page-top" href="#masthead" title="<?php _e('Go to the top of the page', 'frontierline'); ?>"><?php _e('Top', 'frontierline'); ?></a>
+    <?php if (get_theme_mod('frontierline_firefox_button') === '1') : ?>
+      <aside class="nav-global-fxdownload"><a href="https://www.mozilla.org/firefox/new/?utm_source=<?php echo frontierline_blog_domain(); ?>&amp;utm_medium=referral&amp;utm_campaign=blog-nav" rel="external" class="button button-green"><?php _e('Download Firefox', 'frontierline'); ?></a></aside>
+    <?php endif; ?>
+    </div>
   </div>
 </nav>
