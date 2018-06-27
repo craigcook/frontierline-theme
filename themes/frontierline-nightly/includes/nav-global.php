e93db2c655ef51f8f836529e5a0e155f64ca391f
Craig Cook <craigcook@users.noreply.github.com>
2018-06-27 16:31:38 -0700

    Update Nightly global nav

--- themes/frontierline-nightly/includes/nav-global.php
+++ themes/frontierline-nightly/includes/nav-global.php
@@ -6,9 +6,7 @@
 
 <nav id="nav-global" class="nav-global can-stick">
   <div class="content">
-    <div class="logo"><a href="https://www.mozilla.org" rel="external" title="<?php _e('Visit mozilla.org', 'frontierline'); ?>">Mozilla</a></div>
-
-    <p class="fx-cta"><a href="https://www.mozilla.org/firefox/channel/desktop/#nightly" rel="external" class="button button-green"><?php _e('Download Firefox Nightly', 'frontierline'); ?></a></p>
+    <div class="logo"><a href="https://www.mozilla.org/?utm_source=<?php echo frontierline_blog_domain(); ?>&amp;utm_medium=referral&amp;utm_campaign=blog-nav" rel="external" title="<?php _e('Visit mozilla.org', 'frontierline'); ?>">Mozilla</a></div>
 
     <div class="nav-mozilla">
       <span class="toggle" role="button" aria-controls="nav-mozilla-menu" aria-expanded="false" tabindex="0"><?php _e('Menu', 'frontierline'); ?></span>
@@ -16,8 +14,11 @@
         <li class="nav-global-twitter"><a href="https://twitter.com/FirefoxNightly" rel="external"><?php _e('Twitter', 'frontierline'); ?></a></li>
         <li class="nav-global-join"><a href="https://wiki.mozilla.org/Nightly" rel="external"><?php _e('Get Involved', 'frontierline'); ?></a></li>
       </ul>
+
+      <aside class="nav-global-fxdownload">
+        <a href="https://www.mozilla.org/firefox/channel/desktop/?utm_source=<?php echo frontierline_blog_domain(); ?>&amp;utm_medium=referral&amp;utm_campaign=blog-nav#nightly" rel="external" class="button button-green"><?php _e('Download Firefox Nightly', 'frontierline'); ?></a>
+      </aside>
     </div>
 
-    <a class="page-top" href="#masthead" title="<?php _e('Go to the top of the page', 'frontierline'); ?>"><?php _e('Top', 'frontierline'); ?></a>
   </div>
 </nav>
